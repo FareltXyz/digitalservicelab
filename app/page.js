ab82ddd9f7cacaf810be1259ea40332142f46f54
FareltXyz <fareltxyz@gmail.com>
2026-01-31 08:47:37 +0700

    add service

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 "use client"
 import Image from "next/image";
 import { Oswald, Poppins } from "next/font/google";
-import { FaHouse, FaRegMessages, FaCircleInfo, FaPhone, FaMessage } from "react-icons/fa6";
+import { FaHouse, FaRegMessages, FaCircleInfo, FaPhone, FaMessage, FaCheck } from "react-icons/fa6";
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import 'swiper/css';
@@ -11,13 +11,37 @@ import 'swiper/css/navigation';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
 const poppinsFont= Poppins({ display: 'swap', weight: '800', subsets: ['latin']})
+const services = [
+  {
+    name: "Perbaikan Laptop",
+    harga: "75k-350k IDR",
+    list: [
+      "Ganti Ram", "Ganti SSD/HDD", "Ganti Keyboard", "Ganti Touchpad", "Ganti Speaker", "Ganti Baterai"
+    ]
+  },
+  {
+    name: "Perbaikan Hp",
+    harga: "35k IDR",
+    list: [
+      "Pemasangan Tempered Glass", "Reset Setelan Pabrik", "Pembersihan Penyimpanan", "Reset Sandi"
+    ]
+  },
+  {
+    name: "Instalasi WIfi",
+    harga: "350-400k IDR",
+    list: [
+      "Pasang Wifi Rumah", "Ganti Password Wifi", "Penggantian Perangkat Wifi"
+    ]
+  }
+]
+
 
 export default function Home() {
   return (
     <>
       <div className="min-h-screen min-w-screen flex flex-col relative">
         <nav className="fixed z-50 top-0 w-screen">
-          <div className="w-screen h-22 bg-white border flex items-center flex-row justify-between px-4 md:px-24">
+          <div className="w-screen h-22 bg-white max-md:shadow-md max-md:shadow-black/35  md:border flex items-center flex-row justify-between px-4 md:px-24">
             <div className="flex-1 flex items-center gap-3">
               <Image alt="logo" className="rounded-full border border-sky-500" width={50} height={50} src={"/logo.jpeg"}/>
               <p className={`text-sky-500 ${poppinsFont.className} font-bold text-xl`}> DIGITAL SERVICE LAB</p>
@@ -32,7 +56,7 @@ export default function Home() {
             </div>
           </div>
         </nav>
-        <main className="pt-24 md:pb-24 border-b-2 flex h-auto md:min-h-screen w-full bg-white ">
+        <main className="pt-24 md:pb-24 border-b border-black/40  md:border-b-2 flex h-auto md:min-h-screen w-full bg-white ">
           <div className="grid flex-1 grid-cols-1 xl:grid-cols-2 gap-16">
             <section className="flex gap-8 flex-col px-10 md:px-24 justify-center">
               <h1 className="max-w-3xl text-sky-500 text-4xl md:text-6xl font-extrabold">Digital Service Lab. Solusi Digital Cepat, Tepat, dan Terpercaya.</h1>
@@ -76,7 +100,7 @@ export default function Home() {
             </section>
           </div>
         </main>
-        <section id="tentang" className="pt-24 h-auto min-h-screen w-full bg-white px-10">
+        <section id="tentang" className="pt-24 max-md:pb-24 h-auto min-h-screen w-full bg-white px-10">
             <div className="w-full h-full gap-24 flex flex-col items-center">
                 <div className="space-y-7">
                     <h1 className="text-4xl text-cyan-500 font-extrabold text-center">Tentang kami</h1>
@@ -84,23 +108,24 @@ export default function Home() {
                 </div>
                 <div className="space-y-7">
                     <h1 className="text-4xl text-teal-500 font-extrabold text-center"> Layanan Kami </h1>
-                    <div className="grid grid-cols-1 md:grid-cols-2 ">
-                        <div className="flex flex-col rounded-2xl shadow-md shadow-black/50 border w-md p-6">
-                            <div className="md:w-sm mx-auto rounded-full mb-5 h-2 bg-yellow-400 "></div>
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                      { services.map((data, i) => {
+                        return (  
+                          <div className="flex flex-col rounded-2xl shadow-md shadow-black/50 border p-6">
+                            <div className="md:w-xs mx-auto rounded-full mb-5 h-2 bg-yellow-400 "></div>
                             <div className="flex max-md:flex-col justify-between">
-                              <h3 className="text-2xl from-emerald-500 to-sky-500 bg-clip-text bg-linear-to-r font-bold text-transparent">Servis laptop</h3>
-                              <p className="text-xl text-gray-600">75k-350k IDR</p>
+                              <h3 className="text-2xl from-emerald-500 to-sky-500 bg-clip-text bg-linear-to-r md:mx-3 font-bold text-transparent flex-1">{data.name}</h3>
+                              <p className="text-sm my-auto text-gray-600">{data.harga}</p>
                             </div>
-
-                            <ul className="text-gray-700 flex-1 mt-8 mb-4 space-y-2">
-                                <li>Ganti SSD/HDD</li>
-                                <li>Ganti Ram</li>
-                                <li>Ganti Keyboard</li>
-                                <li>Ganti Touchpad</li>
-                                <li>Ganti Speaker</li>
-                                <li>Ganti Baterai</li>
+                            <p className="text-gray-500 text-xl mt-8">Meliputi:</p>
+                            <ul className="text-gray-500 flex-1 max-w-xs mb-4 space-y-2">
+                              {data.list.map((list, i) => {return(<li className="flex items-center gap-3 italic"><FaCheck />{list}</li>)})}
+                                
+                            
                             </ul>
                         </div>
+                        )
+                      })}
                     </div>
                 </div>
             </div>
